454c13892b428c849a6b27b4749ee34f7dd9b0a8
Minhyo <minhyo17@gmail.com>
2023-12-25 11:19:42 +0900

    first commint

---
+++ src/main/java/team01/studyCm/StudyCmApplication.java
@@ -0,0 +1,13 @@
+package team01.studyCm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team01/studyCm/StudyCmApplicationTests.java
@@ -0,0 +1,13 @@
+package team01.studyCm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
