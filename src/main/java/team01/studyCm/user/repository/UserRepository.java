8ea23c4f2a39cb23ccf809ac7561fd9307d5ae49
woojinpark43 <zzangwooojiin@gmail.com>
2023-12-26 11:11:42 -0500

    small update

--- src/main/java/team01/studyCm/user/repository/UserRepository.java
+++ src/main/java/team01/studyCm/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package team01.studyCm.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team01.studyCm.user.entity.User;
 
+@Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
 }
