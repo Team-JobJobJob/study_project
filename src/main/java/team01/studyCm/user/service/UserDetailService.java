1bb75a564ca1e53618784d711bc9bcf7dcd10ade
[yestruly] <[dpwls143@gmail.com]>
2023-12-28 02:55:57 +0900

    보안 관련

---
+++ src/main/java/team01/studyCm/user/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package team01.studyCm.user.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team01.studyCm.user.repository.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class UserDetailService implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("해당 고객 없음"));
+  }
+}
