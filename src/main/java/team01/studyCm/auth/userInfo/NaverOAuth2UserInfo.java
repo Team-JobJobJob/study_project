5248840c8d3fa219d95f0b4fde547738dfc866c0
[yestruly] <[dpwls143@gmail.com]>
2023-12-30 02:13:58 +0900

    spring security 실행 오류 관련

---
+++ src/main/java/team01/studyCm/auth/userInfo/NaverOAuth2UserInfo.java
@@ -0,0 +1,33 @@
+package team01.studyCm.auth.userInfo;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo{
+
+  public NaverOAuth2UserInfo(Map<String, Object> attributes){
+    super(attributes);
+  }
+
+  //response key로 한 번 감싸져 있어 get("response")로 꺼낸 뒤 사용할 정보 key로 사용
+  @Override
+  public String getId() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("id");
+  }
+
+  @Override
+  public String getName() {
+    Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+    if (response == null) {
+      return null;
+    }
+
+    return (String) response.get("name");
+  }
+}
--- src/main/java/team01/studyCm/config/TokenProvider.java
+++ src/main/java/team01/studyCm/config/TokenProvider.java
@@ -1,43 +1,58 @@
 package team01.studyCm.config;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.Optional;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import team01.studyCm.user.repository.UserRepository;
 import team01.studyCm.user.service.UserDetailService;
 
 @Component
 @RequiredArgsConstructor
+@Getter
+@Slf4j
 public class TokenProvider {
+
   private static final String KEY_ROLES = "role";
   private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; //1h
   private final UserDetailService userDetailService;
+  private final UserRepository userRepository;
 
-
-  @Value("{spring.jwt.secret}")
+  @Value("${jwt.secretKey}")
   private String secretKey;
 
-  public String generateToken(String userId, String role) {
-    Claims claims = Jwts.claims().setSubject(userId);
-    claims.put(KEY_ROLES, role);
+  @Value("${jwt.access.header}")
+  private String accessHeader;
 
-    var now = new Date();
-    var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+  @Value("${jwt.refresh.header}")
+  private String refreshHeader;
+
+  public String createAccessToken(String email) {
+    Date now = new Date();
+    Date expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
 
     return Jwts.builder()
-        .setClaims(claims)
+        .setSubject("AccessToken")
         .setIssuedAt(now)
         .setExpiration(expiredDate)
         .signWith(SignatureAlgorithm.HS512, this.secretKey)
+        .claim("email",email)
         .compact();
 
   }
@@ -47,7 +62,9 @@ public class TokenProvider {
   }
 
   public boolean validateToken(String token) {
-    if (!StringUtils.hasText(token)) return false;
+    if (!StringUtils.hasText(token)) {
+      return false;
+    }
 
     var claims = this.parseClaims(token);
     return !claims.getExpiration().before(new Date());
@@ -56,7 +73,7 @@ public class TokenProvider {
   private Claims parseClaims(String token) {
     try {
       return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJws(token).getBody();
-    } catch (ExpiredJwtException e){
+    } catch (ExpiredJwtException e) {
       return e.getClaims();
     }
   }
@@ -66,4 +83,84 @@ public class TokenProvider {
     return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
   }
 
+  private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+    response.setHeader(accessHeader, accessToken);
+  }
+
+  //refreshToken
+  public String createRefreshToken() {
+    Date now = new Date();
+    Date expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+    return Jwts.builder()
+        .setSubject("RefreshToken")
+        .setIssuedAt(now)
+        .setExpiration(expiredDate)
+        .signWith(SignatureAlgorithm.HS512, this.secretKey)
+        .compact();
+  }
+
+  public void updateRefreshToken(String email, String refreshToken) {
+    userRepository.findByEmail(email)
+        .ifPresentOrElse(user -> user.updateRefreshToken(refreshToken),
+            () -> new Exception("일치하는 회원이 없습니다"));
+  }
+
+  private void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
+    response.setHeader(refreshHeader, refreshToken);
+
+  }
+
+  public void sendAccessToken(HttpServletResponse response, String accessToken) {
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.setHeader(accessHeader, accessToken);
+    response.setHeader(accessHeader, accessToken);
+  }
+
+  public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken,
+      String refreshToken) {
+    response.setStatus(HttpServletResponse.SC_OK);
+    setAccessTokenHeader(response, accessToken);
+    setRefreshTokenHeader(response, refreshToken);
+
+  }
+
+  // 헤더에서 RefreshToken추출 ->  헤더 가져온 후 Bearer삭제
+  public Optional<String> extractRefreshToken(HttpServletRequest request) {
+    return Optional.ofNullable(request.getHeader(refreshHeader))
+        .filter(refreshToken -> refreshToken.startsWith("Bearer"))
+        .map(refreshToken -> refreshToken.replace("Bearer", ""));
+  }
+
+  //헤더에서 AccessToken추출
+  public Optional<String> extractAccessToken(HttpServletRequest request) {
+    return Optional.ofNullable(request.getHeader(accessHeader))
+        .filter(accessToken -> accessToken.startsWith("Bearer"))
+        .map(accessToken -> accessToken.replace("Bearer",""));
+  }
+
+  public boolean isTokenValid(String token) {
+    try {
+      JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
+      return true;
+    } catch (Exception e) {
+      log.error("유효하지 않은 토큰 ->{}", e.getMessage());
+      return false;
+    }
+  }
+
+  //AccessToken에서 email추출
+  public Optional<String> extractEmail(String accessToken) {
+    try {
+      return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+          .build()
+          .verify(accessToken)
+          .getClaim("email")
+          .asString()
+      );
+    } catch (Exception e) {
+      log.error("유효하지 않은 AccessToken");
+      return Optional.empty();
+    }
+  }
 }
