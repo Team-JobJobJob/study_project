77660dbcdbc562818a684d3399a9eb687c4488d4
Minhyo <minhyo17@gmail.com>
2024-01-03 12:40:06 +0900

    채팅방 생성 기능 구현

--- src/main/java/team01/studyCm/chat/controller/ChatRoomController.java
+++ src/main/java/team01/studyCm/chat/controller/ChatRoomController.java
@@ -1,6 +1,7 @@
 package team01.studyCm.chat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,7 +34,7 @@ public class ChatRoomController {
 
     chatService.createRoom(chatRoomDto);
 
-    return "index";
+    return "chatRooms/createRoomComplete";
   }
 
 }
