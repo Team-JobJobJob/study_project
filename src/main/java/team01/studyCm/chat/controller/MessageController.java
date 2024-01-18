b46ade54dbe0e5633f8aa6a6dec718f6167b3a03
Minhyo <minhyo17@gmail.com>
2024-01-18 23:03:14 +0900

    STOMP를 이용한 채팅 기능 수정

--- src/main/java/team01/studyCm/chat/controller/MessageController.java
+++ src/main/java/team01/studyCm/chat/controller/MessageController.java
@@ -3,16 +3,21 @@ package team01.studyCm.chat.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import team01.studyCm.auth.CustomOAuth2User;
 import team01.studyCm.chat.dto.MessageCreateRequestDto;
 import team01.studyCm.chat.dto.MessageDto;
 import team01.studyCm.chat.entity.Chat;
 import team01.studyCm.chat.repository.ChatRepository;
 import team01.studyCm.chat.service.ChatService;
+import team01.studyCm.config.TokenProvider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +28,10 @@ import java.util.List;
 public class MessageController {
 
     private final SimpMessageSendingOperations messagingTemplate;
+    private final TokenProvider tokenProvider;
+    private final ChatService chatService;
 
-    @Autowired
-    ChatService chatService;
+
 
 //    @MessageMapping(value = "/chat/enter")
 //    public void enter(MessageDto messageDto){
@@ -37,8 +43,10 @@ public class MessageController {
 //    }
 
 
-    @MessageMapping("/chat/message")
-    public void message(MessageDto messageDto) {
+    @MessageMapping("/chat/message/{chatId}")
+    @SendTo("/chat/room/{chatId}")
+    public void contents(@DestinationVariable Long chatId, MessageDto messageDto, CustomOAuth2User oAuth2User) {
+        String loginId = tokenProvider.createAccessToken(oAuth2User.getEmail());
         if (MessageDto.MessageType.ENTER.equals(messageDto.getType())) {
             messageDto.setContents(messageDto.getSender() + "님이 채팅방에 참여하였습니다.");
         }
